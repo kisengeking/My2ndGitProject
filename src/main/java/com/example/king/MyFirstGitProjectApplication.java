358e62275bb56368ba77d6118caa3f7ba9f5bd51
king.kisenge <kisengeking@gmail.com>
2025-05-01 19:41:10 +0100

    Wa Yambo

---
+++ src/main/java/com/example/king/MyFirstGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.king;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstGitProjectApplication.class, args);
+	}
+
+}
