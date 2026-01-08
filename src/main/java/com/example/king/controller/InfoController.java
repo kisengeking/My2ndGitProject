b9fc428a2d2144550c3168e58b69089a5b5bca5e
kisengeking <92384977+kisengeking@users.noreply.github.com>
2026-01-08 19:20:54 +0100

    Update InfoController.java

--- src/main/java/com/example/king/controller/InfoController.java
+++ src/main/java/com/example/king/controller/InfoController.java
@@ -1,8 +1,8 @@
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.net.InetAddress;
 import java.net.NetworkInterface;
@@ -14,6 +14,7 @@ import java.util.Enumeration;
 import java.util.HashMap;
 
 @RestController
+@RequestMapping("/api")
 public class InfoController {
 
     private final Instant startTime;
